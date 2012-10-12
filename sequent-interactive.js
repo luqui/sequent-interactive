d092483a0ccb4f07919dec30367d916f175c710c
Luke Palmer <lrpalmer@gmail.com>
2012-10-12 16:48:53 -0600

    More stuff

--- sequent-interactive.js
+++ sequent-interactive.js
@@ -1,2 +1,37 @@
-SequentInteractiveModule = function($) {
+SequentInteractiveModule = function($, CodeMirror) {
+
+var elt = function(type, attrs) {
+    var ret = $(document.createElement(type));
+    if (attrs) {
+        for (var i in attrs) {
+            ret.attr(i, attrs[i]);
+        }
+    }
+    for (var i = 2; i < arguments.length; i++) {
+        ret.append(arguments[i]);
+    }
+    return ret;
+};
+
+var text = function(text) {
+    return $(document.createTextNode(text));
+};
+
+var $$ = {};
+
+$$.repl = function() {
+    var io = elt('div', { class: 'sequent-io' });
+    var editor = elt('div', { class: 'sequent-editor' } );
+    var mirror = CodeMirror(function(cmelt) {
+        editor.append(cmelt);
+    });
+    io.append(editor);
+    
+    var result = elt('div', { class: 'sequent-output span11 offset1' }, text('Foo!'));
+    io.append(result);
+    return io;
+};
+
+return $$;
+
 };
